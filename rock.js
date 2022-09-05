ba37a30913b3bf47264e37277d2d288a28a57b4e
Papa1822 <mikepaps1822000@gmail.com>
2022-09-05 23:42:12 +0000

    Assign a function that defined the computer choice logic

--- rock.js
+++ rock.js
@@ -13,3 +13,36 @@ possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('cli
 
 
 }))
+function generateComputerChoice(){
+    const randomNumber =Math.floor(Math.random() * 3) + 1;
+    if(randomNumber === 1){
+        computerChoice = 'rock';
+    }else if(randomNumber === 2){
+        computerChoice = 'paper';    
+    }else if(randomNumber === 3){
+        computerChoice = 'scissors';
+    }
+    computerChoiceDisplay.innerHTML = computerChoice;
+}
+function  getResult(){
+    if(userChoice === 'rock' && computerChoice === 'rock'){
+        result = 'Its a draw';
+    }else if(userChoice === 'rock' && computerChoice === 'paper'){
+        result = 'You lose';
+    }else if(userChoice === 'rock' && computerChoice === 'scissors'){
+        result = 'You win';
+    }else if(userChoice === 'paper' && computerChoice === 'rock' ){
+        result = 'You win';
+    }else if(userChoice === 'paper' && computerChoice === 'paper'){
+        result = 'Its a draw';
+    }else if(userChoice === 'paper' && computerChoice === 'scissors'){
+        result = 'You lose';
+    }else if(userChoice === 'scissors' && computerChoice === 'rock'){
+        result = 'You lose';
+    }else if(userChoice === 'scissors' && computerChoice === 'paper'){
+        result = 'You win';
+    }else if(userChoice === 'scissors' && computerChoice === 'scissors'){
+        result = 'Its a draw';
+    }
+    resultDisplay.innerHTML = result;
+}
